r-smith/deceptifeed

commit 12ada38faad6fa5172ba54ceec7fccc1d5eca829

Change http header name logging to lowercase
This change converts HTTP header names to lower case when writing to JSON logs. This ensures consistent casing across all log fields.

internal/httpserver/httpserver.go
@@ -267,16 +267,17 @@ func parseCustomHeaders(headers string) map[string]string {
 
 // flattenHeaders converts HTTP headers from an http.Request from the format of
 // map[string][]string to map[string]string. This results in a cleaner format
-// for logging, where each headers values are represented as a single string
+// for logging, where each header's values are represented as a single string
 // instead of a slice. When a header contains multiple values, they are
-// combined into a single string, separated by commas.
+// combined into a single string, separated by commas. Additionally, header
+// names are converted to lowercase.
 func flattenHeaders(headers map[string][]string) map[string]string {
 	newHeaders := make(map[string]string, len(headers))
 	for header, values := range headers {
 		if len(values) == 1 {
-			newHeaders[header] = values[0]
+			newHeaders[strings.ToLower(header)] = values[0]
 		} else {
-			newHeaders[header] = "[" + strings.Join(values, ", ") + "]"
+			newHeaders[strings.ToLower(header)] = "[" + strings.Join(values, ", ") + "]"
 		}
 	}
 	// Delete the User-Agent header, as it is managed separately.

commit 1df1a045d0066ec5092153d0ab62dd3ede936ac4

Fix: Don't log "User-Agent" in `headers` key
This commit ensures the `User-Agent` value is removed from the HTTP request headers when logging the request. `User-Agent` is logged in the `event_details` array, so it should not duplicate the value in the `headers` array when logging. This commit fixes the issue introduced in commit 12ada38faad6fa5172ba54ceec7fccc1d5eca829 which normalised header names to lowercase.

internal/httpserver/httpserver.go
@@ -280,7 +280,7 @@ func flattenHeaders(headers map[string][]string) map[string]string {
 		}
 	}
 	// Delete the User-Agent header, as it is managed separately.
-	delete(newHeaders, "User-Agent")
+	delete(newHeaders, "user-agent")
 	return newHeaders
 }
 