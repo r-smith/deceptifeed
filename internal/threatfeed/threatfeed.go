f7715f43179a5aec35a9fd56588ba01319edf436
Ryan Smith <22605441+r-smith@users.noreply.github.com>
2026-02-11 23:06:01 -0800

    refactor(feed): combine feed.go with threatfeed.go; delete feed.go
    - Move everything from feed.go into threatfeed.go.
    - Rename 'd' receivers to 'tdb' for better clarity.

--- internal/threatfeed/threatfeed.go
+++ internal/threatfeed/threatfeed.go
@@ -2,17 +2,21 @@ package threatfeed
 
 import (
 	"bufio"
+	"cmp"
 	"encoding/csv"
 	"errors"
 	"fmt"
 	"io"
 	"net/netip"
 	"os"
+	"slices"
 	"strconv"
 	"strings"
 	"sync"
 	"sync/atomic"
 	"time"
+
+	"github.com/r-smith/deceptifeed/internal/stix"
 )
 
 const (
@@ -96,9 +100,9 @@ func Update(ip netip.Addr) {
 
 // loadCSV populates the in-memory threatfeed with existing records from a CSV
 // file. It restores the threatfeed state during startup.
-func (d *threatDB) loadCSV() error {
-	d.Lock()
-	defer d.Unlock()
+func (tdb *threatDB) loadCSV() error {
+	tdb.Lock()
+	defer tdb.Unlock()
 
 	f, err := os.Open(cfg.ThreatFeed.DatabasePath)
 	if err != nil {
@@ -156,7 +160,7 @@ func (d *threatDB) loadCSV() error {
 			count = 1
 		}
 
-		d.entries[ip] = &threat{added: added, lastSeen: lastSeen, observations: count}
+		tdb.entries[ip] = &threat{added: added, lastSeen: lastSeen, observations: count}
 	}
 	return nil
 }
@@ -164,11 +168,11 @@ func (d *threatDB) loadCSV() error {
 // saveCSV writes the threatfeed to a CSV file for persistence. This allows the
 // threatfeed to be restored after a restart. It is independent of the live
 // in-memory feed.
-func (d *threatDB) saveCSV() error {
+func (tdb *threatDB) saveCSV() error {
 	// Copy db to a temporary slice, to minimize lock time.
-	d.Lock()
-	tempDB := make([]threatRecord, 0, len(d.entries))
-	for ip, t := range d.entries {
+	tdb.Lock()
+	tempDB := make([]threatRecord, 0, len(tdb.entries))
+	for ip, t := range tdb.entries {
 		tempDB = append(tempDB, threatRecord{
 			IP:           ip,
 			Added:        t.added,
@@ -176,7 +180,7 @@ func (d *threatDB) saveCSV() error {
 			Observations: t.observations,
 		})
 	}
-	d.Unlock()
+	tdb.Unlock()
 
 	// Prepare a temp file.
 	tmpFile := cfg.ThreatFeed.DatabasePath + ".tmp"
@@ -223,7 +227,7 @@ func (d *threatDB) saveCSV() error {
 }
 
 // deleteExpired deletes expired threatfeed entries from the database.
-func (d *threatDB) deleteExpired() {
+func (tdb *threatDB) deleteExpired() {
 	if cfg.ThreatFeed.ExpiryHours <= 0 {
 		return
 	}
@@ -231,18 +235,18 @@ func (d *threatDB) deleteExpired() {
 	cutoff := time.Now().Add(-time.Hour * time.Duration(cfg.ThreatFeed.ExpiryHours))
 	isModified := false
 
-	d.Lock()
-	defer d.Unlock()
+	tdb.Lock()
+	defer tdb.Unlock()
 
-	for ip, t := range d.entries {
+	for ip, t := range tdb.entries {
 		if t.lastSeen.Before(cutoff) {
-			delete(d.entries, ip)
+			delete(tdb.entries, ip)
 			isModified = true
 		}
 	}
 
 	if isModified {
-		d.hasChanged.Store(true)
+		tdb.hasChanged.Store(true)
 	}
 }
 
@@ -253,4 +257,246 @@ func (t *threat) expired(at time.Time) bool {
 		return false
 	}
 	return t.lastSeen.Before(at.Add(-time.Hour * time.Duration(cfg.ThreatFeed.ExpiryHours)))
+}
+
+type (
+	// threatRecord represents a threat entry formatted for client delivery.
+	threatRecord struct {
+		IP           netip.Addr `json:"ip"`
+		Added        time.Time  `json:"added"`
+		LastSeen     time.Time  `json:"last_seen"`
+		Observations int        `json:"observations"`
+	}
+
+	// threatRecords represents the actual threatfeed ready to serve to clients.
+	threatRecords []threatRecord
+
+	// feedOptions define configurable options for serving the threatfeed.
+	feedOptions struct {
+		sortBy     sortMethod
+		descending bool
+		after      time.Time
+		limit      int
+		page       int
+	}
+
+	// sortMethod represents the method used for sorting the threatfeed.
+	sortMethod int
+)
+
+const (
+	byIP sortMethod = iota
+	byAdded
+	byLastSeen
+	byObservations
+)
+
+// snapshot filters and converts the threatfeed database into a slice to serve
+// to clients.
+func (tdb *threatDB) snapshot(options ...feedOptions) threatRecords {
+	// Set default feed options.
+	opt := feedOptions{
+		sortBy:     byIP,
+		descending: false,
+	}
+	// Override default options if provided.
+	if len(options) > 0 {
+		opt = options[0]
+	}
+
+	// Copy entries from threatfeed database into a slice of threatRecords.
+	asOf := time.Now()
+	tdb.Lock()
+	threats := make(threatRecords, 0, len(tdb.entries))
+
+	for ip, t := range tdb.entries {
+		if t.expired(asOf) || !t.lastSeen.After(opt.after) {
+			continue
+		}
+
+		if ip.IsPrivate() && !cfg.ThreatFeed.IsPrivateIncluded {
+			continue
+		}
+
+		if isExcluded(ip) {
+			continue
+		}
+
+		threats = append(threats, threatRecord{
+			IP:           ip,
+			Added:        t.added,
+			LastSeen:     t.lastSeen,
+			Observations: t.observations,
+		})
+	}
+	tdb.Unlock()
+
+	threats.sort(opt.sortBy, opt.descending)
+
+	return threats
+}
+
+// sort sorts threat records based on the specified sort criteria.
+func (t threatRecords) sort(method sortMethod, descending bool) {
+	slices.SortFunc(t, func(a, b threatRecord) int {
+		var t int
+		switch method {
+		case byIP:
+			t = a.IP.Compare(b.IP)
+		case byLastSeen:
+			t = a.LastSeen.Compare(b.LastSeen)
+		case byAdded:
+			t = a.Added.Compare(b.Added)
+		case byObservations:
+			t = cmp.Compare(a.Observations, b.Observations)
+		}
+
+		// If values are equal, sort by IP.
+		if t == 0 && method != byIP {
+			t = a.IP.Compare(b.IP)
+		}
+
+		// Inverse sort if direction is descending.
+		if descending {
+			return t * -1
+		}
+		return t
+	})
+}
+
+// convertToIndicators converts the threatfeed into a collection of STIX
+// STIX Indicator objects.
+func (t threatRecords) convertToIndicators() []stix.Object {
+	if len(t) == 0 {
+		return []stix.Object{}
+	}
+
+	const indicator = "indicator"
+	result := make([]stix.Object, 0, len(t)+1)
+
+	// Add the Deceptifeed `Identity` as the first object in the collection.
+	// All objects in the collection reference this identity as the creator.
+	result = append(result, stix.DeceptifeedIdentity())
+
+	for _, entry := range t {
+		addrType := "ipv4-addr"
+		if entry.IP.Is6() {
+			addrType = "ipv6-addr"
+		}
+		pattern := fmt.Sprintf("[%s:value = '%s']", addrType, entry.IP)
+
+		// Fixed expiration: 2 months since last seen.
+		validUntil := new(time.Time)
+		*validUntil = entry.LastSeen.AddDate(0, 2, 0).UTC()
+
+		// Generate a deterministic identifier using the IP address represented
+		// as a STIX IP pattern and structured as a JSON string. Example:
+		// {"pattern":"[ipv4-addr:value='127.0.0.1']"}
+		patternJSON := fmt.Sprintf("{\"pattern\":\"%s\"}", pattern)
+
+		result = append(result, stix.Indicator{
+			Type:           indicator,
+			SpecVersion:    stix.SpecVersion,
+			ID:             stix.DeterministicID(indicator, patternJSON),
+			IndicatorTypes: []string{"malicious-activity"},
+			Pattern:        pattern,
+			PatternType:    "stix",
+			Created:        entry.Added.UTC(),
+			Modified:       entry.LastSeen.UTC(),
+			ValidFrom:      entry.Added.UTC(),
+			ValidUntil:     validUntil,
+			Name:           "Honeypot interaction: " + entry.IP.String(),
+			Description:    "This IP was observed interacting with a honeypot server.",
+			KillChains:     []stix.KillChain{{KillChain: "mitre-attack", Phase: "reconnaissance"}},
+			Confidence:     100,
+			Lang:           "en",
+			Labels:         []string{"honeypot-interaction"},
+			CreatedByRef:   stix.DeceptifeedID,
+		})
+	}
+	return result
+}
+
+// convertToSightings converts the threatfeed into a collection of STIX
+// Sighting objects.
+func (t threatRecords) convertToSightings() []stix.Object {
+	if len(t) == 0 {
+		return []stix.Object{}
+	}
+
+	const indicator = "indicator"
+	const sighting = "sighting"
+	const maxCount = 999_999_999 // Maximum count according to STIX 2.1 specification.
+	result := make([]stix.Object, 0, len(t)+1)
+
+	// Add the Deceptifeed `Identity` as the first object in the collection.
+	// All objects in the collection reference this identity as the creator.
+	result = append(result, stix.DeceptifeedIdentity())
+
+	for _, entry := range t {
+		addrType := "ipv4-addr"
+		if entry.IP.Is6() {
+			addrType = "ipv6-addr"
+		}
+		pattern := fmt.Sprintf("[%s:value = '%s']", addrType, entry.IP)
+
+		count := min(entry.Observations, maxCount)
+
+		// Generate a deterministic identifier using the IP address represented
+		// as a STIX IP pattern and structured as a JSON string. Example:
+		// {"pattern":"[ipv4-addr:value='127.0.0.1']"}
+		indicatorJSON := fmt.Sprintf("{\"pattern\":\"%s\"}", pattern)
+		indicatorID := stix.DeterministicID(indicator, indicatorJSON)
+
+		result = append(result, stix.Sighting{
+			Type:             sighting,
+			SpecVersion:      stix.SpecVersion,
+			ID:               stix.DeterministicID(sighting, "{\"sighting_of_ref\":\""+indicatorID+"\"}"),
+			Created:          entry.Added.UTC(),
+			Modified:         entry.LastSeen.UTC(),
+			FirstSeen:        entry.Added.UTC(),
+			LastSeen:         entry.LastSeen.UTC(),
+			Count:            count,
+			Description:      "This IP was observed interacting with a honeypot server.",
+			Confidence:       100,
+			Lang:             "en",
+			SightingOfRef:    indicatorID,
+			WhereSightedRefs: []string{stix.DeceptifeedID},
+			CreatedByRef:     stix.DeceptifeedID,
+		})
+	}
+	return result
+}
+
+// convertToObservables converts the threatfeed into a collection of STIX
+// Cyber-observable Objects.
+func (t threatRecords) convertToObservables() []stix.Object {
+	if len(t) == 0 {
+		return []stix.Object{}
+	}
+
+	result := make([]stix.Object, 0, len(t)+1)
+
+	// Add the Deceptifeed `Identity` as the first object in the collection.
+	// All objects in the collection reference this identity as the creator.
+	result = append(result, stix.DeceptifeedIdentity())
+
+	for _, entry := range t {
+		t := "ipv4-addr"
+		if entry.IP.Is6() {
+			t = "ipv6-addr"
+		}
+
+		// Generate a deterministic identifier for each IP address in the
+		// threatfeed using the IP value represented as a JSON string. For
+		// example: {"value":"127.0.0.1"}
+		result = append(result, stix.ObservableIP{
+			Type:         t,
+			SpecVersion:  stix.SpecVersion,
+			ID:           stix.DeterministicID(t, "{\"value\":\""+entry.IP.String()+"\"}"),
+			Value:        entry.IP.String(),
+			CreatedByRef: stix.DeceptifeedID,
+		})
+	}
+	return result
 }
